70a48dc56b0103846433015264ef6491011cd025
dan <dan@master>
2020-11-12 15:19:55 -0600

    Initial check in

---
+++ DanApp/src/main/java/com/danapp/DanAppApplication.java
@@ -0,0 +1,13 @@
+package com.danapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanAppApplication.class, args);
+	}
+
+}
---
+++ DanApp/src/test/java/com/danapp/DanAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
